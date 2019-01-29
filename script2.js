b4b3883eeb3c1a6f485a7df0dcea65ea6a7b85f0
AndreasAanestad <43755999+AndreasAanestad@users.noreply.github.com>
2019-01-29 22:21:06 +0100

    Initial commit

---
+++ script2.js
@@ -0,0 +1,838 @@
+
+
+
+
+ const multiplicationtable = {
+     cheese: 42,
+     items: [{
+         name: "1x1",
+         multiplikator: 1,
+         multiplikand: 1,
+         score: 2
+     }, {
+       name: "1x2",
+       multiplikator: 1,
+       multiplikand: 2,
+       score: 2
+     }, {
+       name: "1x3",
+       multiplikator: 1,
+       multiplikand: 3,
+       score: 2
+     }, {
+       name: "1x4",
+       multiplikator: 1,
+       multiplikand: 4,
+       score: 2
+     }, {
+       name: "1x5",
+       multiplikator: 1,
+       multiplikand: 5,
+       score: 2
+     }, {
+       name: "1x6",
+       multiplikator: 1,
+       multiplikand: 6,
+       score: 2
+     }, {
+       name: "1x7",
+       multiplikator: 1,
+       multiplikand: 7,
+       score: 2
+     }, {
+       name: "1x8",
+       multiplikator: 1,
+       multiplikand: 8,
+       score: 2
+     }, {
+       name: "1x9",
+       multiplikator: 1,
+       multiplikand: 9,
+       score: 2
+     }, {
+       name: "1x10",
+       multiplikator: 1,
+       multiplikand: 10,
+       score: 2
+     }, {
+       name: "2x1",
+       multiplikator: 2,
+       multiplikand: 1,
+       score: 2
+     }, {
+       name: "2x2",
+       multiplikator: 2,
+       multiplikand: 2,
+       score: 2
+     }, {
+       name: "2x3",
+       multiplikator: 2,
+       multiplikand: 3,
+       score: 2
+     }, {
+       name: "2x4",
+       multiplikator: 2,
+       multiplikand: 4,
+       score: 2
+     }, {
+       name: "2x5",
+       multiplikator: 2,
+       multiplikand: 5,
+       score: 2
+     }, {
+       name: "2x6",
+       multiplikator: 2,
+       multiplikand: 6,
+       score: 2
+     }, {
+       name: "2x7",
+       multiplikator: 2,
+       multiplikand: 7,
+       score: 2
+     }, {
+       name: "2x8",
+       multiplikator: 2,
+       multiplikand: 8,
+       score: 2
+     }, {
+       name: "2x9",
+       multiplikator: 2,
+       multiplikand: 9,
+       score: 2
+     }, {
+       name: "2x10",
+       multiplikator: 2,
+       multiplikand: 10,
+       score: 2
+     }, {
+       name: "3x1",
+       multiplikator: 3,
+       multiplikand: 1,
+       score: 2
+     }, {
+       name: "3x2",
+       multiplikator: 3,
+       multiplikand: 2,
+       score: 2
+     }, {
+       name: "3x3",
+       multiplikator: 3,
+       multiplikand: 3,
+       score: 2
+     }, {
+       name: "3x4",
+       multiplikator: 3,
+       multiplikand: 4,
+       score: 2
+     }, {
+       name: "3x5",
+       multiplikator: 3,
+       multiplikand: 5,
+       score: 2
+     }, {
+       name: "3x6",
+       multiplikator: 3,
+       multiplikand: 6,
+       score: 2
+     }, {
+       name: "3x7",
+       multiplikator: 3,
+       multiplikand: 7,
+       score: 2
+     }, {
+       name: "3x8",
+       multiplikator: 3,
+       multiplikand: 8,
+       score: 2
+     }, {
+       name: "3x9",
+       multiplikator: 3,
+       multiplikand: 9,
+       score: 2
+     }, {
+       name: "3x10",
+       multiplikator: 3,
+       multiplikand: 10,
+       score: 2
+     }, {
+       name: "4x1",
+       multiplikator: 4,
+       multiplikand: 1,
+       score: 2
+     }, {
+       name: "4x2",
+       multiplikator: 4,
+       multiplikand: 2,
+       score: 2
+     }, {
+       name: "4x3",
+       multiplikator: 4,
+       multiplikand: 3,
+       score: 2
+     }, {
+       name: "4x4",
+       multiplikator: 4,
+       multiplikand: 4,
+       score: 2
+     }, {
+       name: "4x5",
+       multiplikator: 4,
+       multiplikand: 5,
+       score: 2
+     }, {
+       name: "4x6",
+       multiplikator: 4,
+       multiplikand: 6,
+       score: 2
+     }, {
+       name: "4x7",
+       multiplikator: 4,
+       multiplikand: 7,
+       score: 2
+     }, {
+       name: "4x8",
+       multiplikator: 4,
+       multiplikand: 8,
+       score: 2
+     }, {
+       name: "4x9",
+       multiplikator: 4,
+       multiplikand: 9,
+       score: 2
+     }, {
+       name: "4x10",
+       multiplikator: 4,
+       multiplikand: 10,
+       score: 2
+     }, {
+       name: "5x1",
+       multiplikator: 5,
+       multiplikand: 1,
+       score: 2
+     }, {
+       name: "5x2",
+       multiplikator: 5,
+       multiplikand: 2,
+       score: 2
+     }, {
+       name: "5x3",
+       multiplikator: 5,
+       multiplikand: 3,
+       score: 2
+     }, {
+       name: "5x4",
+       multiplikator: 5,
+       multiplikand: 4,
+       score: 2
+     }, {
+       name: "5x5",
+       multiplikator: 5,
+       multiplikand: 5,
+       score: 2
+     }, {
+       name: "5x6",
+       multiplikator: 2,
+       multiplikand: 6,
+       score: 2
+     }, {
+       name: "5x7",
+       multiplikator: 5,
+       multiplikand: 7,
+       score: 2
+     }, {
+       name: "5x8",
+       multiplikator: 5,
+       multiplikand: 8,
+       score: 2
+     }, {
+       name: "5x9",
+       multiplikator: 5,
+       multiplikand: 9,
+       score: 2
+     }, {
+       name: "5x10",
+       multiplikator: 5,
+       multiplikand: 10,
+       score: 2
+     }, {
+       name: "6x1",
+       multiplikator: 6,
+       multiplikand: 1,
+       score: 2
+     }, {
+       name: "6x2",
+       multiplikator: 6,
+       multiplikand: 2,
+       score: 2
+     }, {
+       name: "6x3",
+       multiplikator: 6,
+       multiplikand: 3,
+       score: 2
+     }, {
+       name: "6x4",
+       multiplikator: 6,
+       multiplikand: 4,
+       score: 2
+     }, {
+       name: "6x5",
+       multiplikator: 6,
+       multiplikand: 5,
+       score: 2
+     }, {
+       name: "6x6",
+       multiplikator: 6,
+       multiplikand: 6,
+       score: 2
+     }, {
+       name: "6x7",
+       multiplikator: 6,
+       multiplikand: 7,
+       score: 2
+     }, {
+       name: "6x8",
+       multiplikator: 6,
+       multiplikand: 8,
+       score: 2
+     }, {
+       name: "6x9",
+       multiplikator: 6,
+       multiplikand: 9,
+       score: 2
+     }, {
+       name: "6x10",
+       multiplikator: 6,
+       multiplikand: 10,
+       score: 2
+     }, {
+       name: "7x1",
+       multiplikator: 7,
+       multiplikand: 1,
+       score: 2
+     }, {
+       name: "7x2",
+       multiplikator: 7,
+       multiplikand: 2,
+       score: 2
+     }, {
+       name: "7x3",
+       multiplikator: 7,
+       multiplikand: 3,
+       score: 2
+     }, {
+       name: "7x4",
+       multiplikator: 7,
+       multiplikand: 4,
+       score: 2
+     }, {
+       name: "7x5",
+       multiplikator: 7,
+       multiplikand: 5,
+       score: 2
+     }, {
+       name: "7x6",
+       multiplikator: 7,
+       multiplikand: 6,
+       score: 2
+     }, {
+       name: "7x7",
+       multiplikator: 7,
+       multiplikand: 7,
+       score: 2
+     }, {
+       name: "7x8",
+       multiplikator: 7,
+       multiplikand: 8,
+       score: 2
+     }, {
+       name: "7x9",
+       multiplikator: 7,
+       multiplikand: 9,
+       score: 2
+     }, {
+       name: "7x10",
+       multiplikator: 7,
+       multiplikand: 10,
+       score: 2
+     }, {
+       name: "8x1",
+       multiplikator: 8,
+       multiplikand: 1,
+       score: 2
+     }, {
+       name: "8x2",
+       multiplikator: 8,
+       multiplikand: 2,
+       score: 2
+     }, {
+       name: "8x3",
+       multiplikator: 8,
+       multiplikand: 3,
+       score: 2
+     }, {
+       name: "8x4",
+       multiplikator: 8,
+       multiplikand: 4,
+       score: 2
+     }, {
+       name: "8x5",
+       multiplikator: 8,
+       multiplikand: 5,
+       score: 2
+     }, {
+       name: "8x6",
+       multiplikator: 8,
+       multiplikand: 6,
+       score: 2
+     }, {
+       name: "8x7",
+       multiplikator: 8,
+       multiplikand: 7,
+       score: 2
+     }, {
+       name: "8x8",
+       multiplikator: 8,
+       multiplikand: 8,
+       score: 2
+     }, {
+       name: "8x9",
+       multiplikator: 8,
+       multiplikand: 9,
+       score: 2
+     }, {
+       name: "8x10",
+       multiplikator: 8,
+       multiplikand: 10,
+       score: 2
+     }, {
+       name: "9x1",
+       multiplikator: 9,
+       multiplikand: 1,
+       score: 2
+     }, {
+       name: "9x2",
+       multiplikator: 9,
+       multiplikand: 2,
+       score: 2
+     }, {
+       name: "9x3",
+       multiplikator: 9,
+       multiplikand: 3,
+       score: 2
+     }, {
+       name: "9x4",
+       multiplikator: 9,
+       multiplikand: 4,
+       score: 2
+     }, {
+       name: "9x5",
+       multiplikator: 9,
+       multiplikand: 5,
+       score: 2
+     }, {
+       name: "9x6",
+       multiplikator: 9,
+       multiplikand: 6,
+       score: 2
+     }, {
+       name: "9x7",
+       multiplikator: 9,
+       multiplikand: 7,
+       score: 2
+     }, {
+       name: "9x8",
+       multiplikator: 9,
+       multiplikand: 8,
+       score: 2
+     }, {
+       name: "9x9",
+       multiplikator: 9,
+       multiplikand: 9,
+       score: 2
+     }, {
+       name: "9x10",
+       multiplikator: 9,
+       multiplikand: 10,
+       score: 2
+     }, {
+       name: "10x1",
+       multiplikator: 10,
+       multiplikand: 1,
+       score: 2
+     }, {
+       name: "10x2",
+       multiplikator: 10,
+       multiplikand: 2,
+       score: 2
+     }, {
+       name: "10x3",
+       multiplikator: 10,
+       multiplikand: 3,
+       score: 2
+     }, {
+       name: "10x4",
+       multiplikator: 10,
+       multiplikand: 4,
+       score: 2
+     }, {
+       name: "10x2",
+       multiplikator: 10,
+       multiplikand: 2,
+       score: 2
+     }, {
+       name: "10x6",
+       multiplikator: 10,
+       multiplikand: 6,
+       score: 2
+     }, {
+       name: "10x7",
+       multiplikator: 10,
+       multiplikand: 7,
+       score: 2
+     }, {
+       name: "10x8",
+       multiplikator: 10,
+       multiplikand: 8,
+       score: 2
+     }, {
+       name: "10x9",
+       multiplikator: 10,
+       multiplikand: 9,
+       score: 2
+     }, {
+       name: "10x10",
+       multiplikator: 10,
+       multiplikand: 10,
+       score: 2
+     }]
+ };
+
+
+var input = document.getElementById("answerbox");
+var currentAnswer = 0;
+var randomProblem = 0;
+var a = 0;
+var b = 0;
+var currentScore = 2;
+var newscore = 0;
+var difficult = 4;
+var timerEnabled = false;
+var timerAmount = 10000;
+var timer;
+var locked = 0;
+var intelligentScore = difficult * 100;
+var victory = false;
+
+var lockedAudio = new Audio("locksound.mp3");
+var correctAudio = new Audio("correctsound.mp3");
+var wrongAudio = new Audio("wrongsound.mp3");
+
+
+
+//Functions that start right away
+newProblem();
+startEasy()
+
+
+
+
+
+
+
+
+
+
+
+function startTimed(){
+timerEnabled = true;
+clearTimeout(timer);
+timer = setTimeout(checkProblem, timerAmount);
+}
+
+
+
+
+function timerTips(){
+
+alert("There is no need to press enter in timed mode, but it might speed things up.");
+
+
+
+}
+
+
+function newProblem(){
+
+  if (timerEnabled == true){
+      startTimed();
+    }
+
+
+
+document.getElementById("answerbox").value = ""
+
+randomProblem = Math.floor((Math.random() * 100) + 0);
+a = multiplicationtable.items[[randomProblem]].multiplikator;
+b = multiplicationtable.items[[randomProblem]].multiplikand;
+currentScore = multiplicationtable.items[[randomProblem]].score;
+newscore = currentScore;
+
+
+//creates a new problem if it has a high score
+while (newscore >= difficult && victory == false){
+randomProblem = Math.floor((Math.random() * 100) + 0);
+a = multiplicationtable.items[[randomProblem]].multiplikator;
+b = multiplicationtable.items[[randomProblem]].multiplikand;
+currentScore = multiplicationtable.items[[randomProblem]].score;
+newscore = currentScore;
+
+
+}
+
+
+
+  var text = a * b ;
+  console.log(text);
+
+  var output = document.getElementById('answerParagraph');
+  output.innerHTML = a + " x " + b;
+
+//for testing
+//document.getElementById("answerbox").value = a * b;
+
+
+
+}
+
+
+function startEasy(){
+difficult = 4;
+timerAmount = 10000;
+checkLocked();
+}
+
+
+function startHard(){
+difficult = 5;
+timerAmount = 5000;
+checkLocked();
+}
+
+function startMaster(){
+difficult = 6;
+timerAmount = 2750;
+checkLocked();
+}
+
+
+
+
+
+
+function checkProblem(){
+var correctAnswer = a * b;
+currentAnswer = document.getElementById("answerbox").value;
+var checker = document.getElementById("correctParagraph");
+if (currentAnswer == correctAnswer){
+checker.style = "color:green; font-weight: bold;";
+checker.innerHTML = "Correct";
+currentScore ++;
+multiplicationtable.items[[randomProblem]].score = currentScore;
+
+if(currentScore < difficult){
+correctAudio.play();
+}else if (currentScore >= difficult) {
+  lockedAudio.play();
+
+}
+
+
+  newProblem();
+}else{
+currentScore --;
+multiplicationtable.items[[randomProblem]].score = currentScore;
+checker.style = "color:red; font-weight: bold;";
+checker.innerHTML = "Wrong";
+wrongAudio.play();
+if (timerEnabled == true){
+
+newProblem();
+
+}
+
+
+}
+
+
+scoreUpdate();
+checkLocked()
+}
+
+
+
+
+
+
+// Get the input field
+var input = document.getElementById("answerbox")
+
+// Execute a function when the user releases a key on the keyboard
+input.addEventListener("keyup", function(event) {
+  // Cancel the default action, if needed
+  event.preventDefault();
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Trigger the button element with a click
+    currentAnswer= document.getElementById("answerbox").value;
+    checkProblem();
+  }
+});
+
+
+
+function checkLocked(){
+locked = 0;
+var visualLocked = "";
+var hundre = 99;
+document.getElementById("lockedDiv").innerHTML = "";
+while(hundre > 0){
+var currentScoreCheck = multiplicationtable.items[[hundre]].score;
+if(currentScoreCheck >= difficult){
+locked ++;
+var x = document.createElement("IMG");
+x.setAttribute("src", "lockedstar.png");
+x.setAttribute("width", "100");
+x.setAttribute("height", "100");
+x.setAttribute("alt", "Locked");
+x.setAttribute("class", "lockedimg");
+document.getElementById("lockedDiv").appendChild(x);
+}
+hundre --;
+}
+
+
+
+
+}
+
+
+
+
+
+
+
+function scoreUpdate(){
+
+intelligentScore =
+  multiplicationtable.items[0].score +
+  multiplicationtable.items[1].score +
+  multiplicationtable.items[2].score +
+  multiplicationtable.items[3].score +
+  multiplicationtable.items[4].score +
+  multiplicationtable.items[5].score +
+  multiplicationtable.items[6].score +
+  multiplicationtable.items[7].score +
+  multiplicationtable.items[8].score +
+  multiplicationtable.items[9].score +
+  multiplicationtable.items[10].score +
+  multiplicationtable.items[11].score +
+  multiplicationtable.items[12].score +
+  multiplicationtable.items[13].score +
+  multiplicationtable.items[14].score +
+  multiplicationtable.items[15].score +
+  multiplicationtable.items[16].score +
+  multiplicationtable.items[17].score +
+  multiplicationtable.items[18].score +
+  multiplicationtable.items[19].score +
+  multiplicationtable.items[20].score +
+  multiplicationtable.items[21].score +
+  multiplicationtable.items[22].score +
+  multiplicationtable.items[23].score +
+  multiplicationtable.items[24].score +
+  multiplicationtable.items[25].score +
+  multiplicationtable.items[26].score +
+  multiplicationtable.items[27].score +
+  multiplicationtable.items[28].score +
+  multiplicationtable.items[29].score +
+  multiplicationtable.items[30].score +
+  multiplicationtable.items[31].score +
+  multiplicationtable.items[32].score +
+  multiplicationtable.items[33].score +
+  multiplicationtable.items[34].score +
+  multiplicationtable.items[35].score +
+  multiplicationtable.items[36].score +
+  multiplicationtable.items[37].score +
+  multiplicationtable.items[38].score +
+  multiplicationtable.items[39].score +
+  multiplicationtable.items[40].score +
+  multiplicationtable.items[41].score +
+  multiplicationtable.items[42].score +
+  multiplicationtable.items[43].score +
+  multiplicationtable.items[44].score +
+  multiplicationtable.items[45].score +
+  multiplicationtable.items[46].score +
+  multiplicationtable.items[47].score +
+  multiplicationtable.items[48].score +
+  multiplicationtable.items[49].score +
+  multiplicationtable.items[50].score +
+  multiplicationtable.items[51].score +
+  multiplicationtable.items[52].score +
+  multiplicationtable.items[53].score +
+  multiplicationtable.items[54].score +
+  multiplicationtable.items[55].score +
+  multiplicationtable.items[56].score +
+  multiplicationtable.items[57].score +
+  multiplicationtable.items[58].score +
+  multiplicationtable.items[59].score +
+  multiplicationtable.items[60].score +
+  multiplicationtable.items[61].score +
+  multiplicationtable.items[62].score +
+  multiplicationtable.items[63].score +
+  multiplicationtable.items[64].score +
+  multiplicationtable.items[65].score +
+  multiplicationtable.items[66].score +
+  multiplicationtable.items[67].score +
+  multiplicationtable.items[68].score +
+  multiplicationtable.items[69].score +
+  multiplicationtable.items[70].score +
+  multiplicationtable.items[71].score +
+  multiplicationtable.items[72].score +
+  multiplicationtable.items[73].score +
+  multiplicationtable.items[74].score +
+  multiplicationtable.items[75].score +
+  multiplicationtable.items[76].score +
+  multiplicationtable.items[77].score +
+  multiplicationtable.items[78].score +
+  multiplicationtable.items[79].score +
+  multiplicationtable.items[80].score +
+  multiplicationtable.items[81].score +
+  multiplicationtable.items[82].score +
+  multiplicationtable.items[83].score +
+  multiplicationtable.items[84].score +
+  multiplicationtable.items[85].score +
+  multiplicationtable.items[86].score +
+  multiplicationtable.items[87].score +
+  multiplicationtable.items[88].score +
+  multiplicationtable.items[89].score +
+  multiplicationtable.items[90].score +
+  multiplicationtable.items[91].score +
+  multiplicationtable.items[92].score +
+  multiplicationtable.items[93].score +
+  multiplicationtable.items[94].score +
+  multiplicationtable.items[95].score +
+  multiplicationtable.items[96].score +
+  multiplicationtable.items[97].score +
+  multiplicationtable.items[98].score +
+  multiplicationtable.items[99].score;
+
+  document.getElementById("score").innerHTML = "You smart-score is :" + intelligentScore;
+
+var checkVictory = 0;
+checkVictory = (difficult * 100)-1;
+if (intelligentScore == checkVictory){
+victory = true;
+}
+
+console.log(intelligentScore);
+console.log(checkVictory);
+}
